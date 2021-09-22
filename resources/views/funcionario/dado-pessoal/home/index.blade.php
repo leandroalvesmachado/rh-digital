27c09af053a61868da365b599015d5d016934055
leandroalvesmachado <leandroalvesmachado@gmail.com>
2021-09-22 16:59:32 -0300

    iniciando card dados pessoais

--- resources/views/funcionario/dado-pessoal/home/index.blade.php
+++ resources/views/funcionario/dado-pessoal/home/index.blade.php
@@ -79,6 +79,119 @@
 @if (!Auth::user()->funcionario)
 <a href="{{ route('funcionario.dado-pessoal.identificacoes.create') }}">Identificação</a>
 @else
+
+<div class="row">
+    <div class="col-md-6 col-lg-6 col-xl-6">
+        <div class="card">
+            <div class="card-header">
+                <i class="fas fa-id-badge"></i> Identificação
+            </div>
+            <div class="card-body">2</div>
+            <div class="card-footer text-muted">3</div>
+        </div>
+    </div>
+    <div class="col-md-6 col-lg-6 col-xl-6">
+        <div class="card">
+            <div class="card-header">
+                <i class="fas fa-address-book"></i> Contatos
+            </div>
+            <div class="card-body">2</div>
+            <div class="card-footer text-muted">3</div>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-6 col-lg-6 col-xl-6">
+        <div class="card">
+            <div class="card-header">
+                <i class="fas fa-users"></i> Dependentes
+            </div>
+            <div class="card-body">2</div>
+            <div class="card-footer text-muted">3</div>
+        </div>
+    </div>
+    <div class="col-md-6 col-lg-6 col-xl-6">
+        <div class="card">
+            <div class="card-header">
+                <i class="fas fa-map-marked-alt"></i> Endereços
+            </div>
+            <div class="card-body">2</div>
+            <div class="card-footer text-muted">3</div>
+        </div>
+    </div>
+</div>
+
+<!-- <div class="row">
+        <div class="col-lg-12 text-center">
+            <h2 class="section-title">Dados Pessoais</h2>
+        </div>
+        <div class="col-lg-3 col-sm-6 mb-4">
+            <div class="card border-0 shadow rounded-xs pt-5">
+                <div class="card-body"> <i class="fa fa-object-ungroup icon-lg icon-primary icon-bg-primary icon-bg-circle mb-3"></i>
+                    <h4 class="mt-4 mb-3">Networking</h4>
+                    <p>For what reason would it be advisable for me to think about business content?</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6 mb-4">
+            <div class="card border-0 shadow rounded-xs pt-5">
+                <div class="card-body"> <i class="fa fa-users icon-lg icon-yellow icon-bg-yellow icon-bg-circle mb-3"></i>
+                    <h4 class="mt-4 mb-3">Social Activity</h4>
+                    <p>For what reason would it be advisable for me to think about business content?</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6 mb-4">
+            <div class="card border-0 shadow rounded-xs pt-5">
+                <div class="card-body"> <i class="fa fa-desktop icon-lg icon-purple icon-bg-purple icon-bg-circle mb-3"></i>
+                    <h4 class="mt-4 mb-3">Web Design</h4>
+                    <p>For what reason would it be advisable for me to think about business content?</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6 mb-4">
+            <div class="card border-0 shadow rounded-xs pt-5">
+                <div class="card-body"> <i class="fa fa-cloud icon-lg icon-cyan icon-bg-cyan icon-bg-circle mb-3"></i>
+                    <h4 class="mt-4 mb-3">Cloud Service</h4>
+                    <p>For what reason would it be advisable for me to think about business content?</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6 mb-4">
+            <div class="card border-0 shadow rounded-xs pt-5">
+                <div class="card-body"> <i class="fa fa-comments icon-lg icon-red icon-bg-red icon-bg-circle mb-3"></i>
+                    <h4 class="mt-4 mb-3">Consulting</h4>
+                    <p>For what reason would it be advisable for me to think about business content?</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6 mb-4">
+            <div class="card border-0 shadow rounded-xs pt-5">
+                <div class="card-body"> <i class="fa fa-search-plus icon-lg icon-green icon-bg-green icon-bg-circle mb-3"></i>
+                    <h4 class="mt-4 mb-3">SEO Optimization</h4>
+                    <p>For what reason would it be advisable for me to think about business content?</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6 mb-4">
+            <div class="card border-0 shadow rounded-xs pt-5">
+                <div class="card-body"> <i class="fa fa-user icon-lg icon-orange icon-bg-orange icon-bg-circle mb-3"></i>
+                    <h4 class="mt-4 mb-3">Usability Testing</h4>
+                    <p>For what reason would it be advisable for me to think about business content?</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6 mb-4">
+            <div class="card border-0 shadow rounded-xs pt-5">
+                <div class="card-body"> <i class="fa fa-envelope icon-lg icon-blue icon-bg-blue icon-bg-circle mb-3"></i>
+                    <h4 class="mt-4 mb-3">UX Prototyping</h4>
+                    <p>For what reason would it be advisable for me to think about business content?</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
 <a href="{{ route('funcionario.dado-pessoal.identificacoes.edit', Auth::user()->funcionario) }}">Identificação Atualizar</a>
 
 <br>
@@ -88,7 +201,7 @@
 <a href="{{ route('funcionario.dado-pessoal.dependentes.create', Auth::user()->funcionario) }}">Dependentes</a>
 
 <br>
-<a href="{{ route('funcionario.dado-pessoal.enderecos.create', Auth::user()->funcionario) }}">Endereços</a>
+<a href="{{ route('funcionario.dado-pessoal.enderecos.create', Auth::user()->funcionario) }}">Endereços</a> -->
 @endif
 
 @endsection
